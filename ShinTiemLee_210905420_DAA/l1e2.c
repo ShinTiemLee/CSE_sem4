5339e5831a124830aa8504cc7f5e096409ae319e
Shin Tiem Lee <96584919+ShinTiemLee@users.noreply.github.com>
2023-01-30 15:34:22 +0530

    updates for daa

---
+++ ShinTiemLee_210905420_DAA/l1e2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int row,col;
+	char let='A';
+	printf("Enter the number of vertices\n");
+	scanf("%d",&row);
+	col=row;
+	int mat[row][col];
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			printf("Enter the adjacency of %c with %c:\n",let+i,let+j);
+			scanf("%d",&mat[i][j]);
+		}
+	}
+	for(int i=0;i<row;i++){
+		printf("\n%c",let+i);
+		for(int j=0;j<col;j++){
+			if(mat[i][j]==1)
+				printf("->%c",let+j);
+		}
+	}
+	return 0;
+}
